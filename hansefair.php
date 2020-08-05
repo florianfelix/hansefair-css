385f0e4531bfb5592fb93fd1890f319668e8637a
florianfelix <florianfelixmeyer@gmail.com>
2020-08-05 15:56:57 +0200

    update desc

--- hansefair.php
+++ hansefair.php
@@ -15,7 +15,7 @@
  * @wordpress-plugin
  * Plugin Name:       Hansefair Custom
  * Plugin URI:        https://github.com/florianfelix/hansefair
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       Custom CSS JS
  * Version:           1.0.0
  * Author:            Florian Felix Meyer
  * Author URI:        https://github.com/florianfelix
