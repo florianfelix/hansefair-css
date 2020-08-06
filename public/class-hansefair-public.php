70bf32e237e62939849f5e9d0fcd8071159360ef
florianfelix <florianfelixmeyer@gmail.com>
2020-08-06 15:39:13 +0200

    set paths 1

--- public/class-hansefair-public.php
+++ public/class-hansefair-public.php
@@ -73,7 +73,8 @@ class Hansefair_Public {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/hansefair-public.css', array(), $this->version, 'all' );
+		// wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/hansefair-public.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'assets/hansefair.css', array(), $this->version, 'all' );
 
 	}
 
@@ -96,7 +97,8 @@ class Hansefair_Public {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/hansefair-public.js', array( 'jquery' ), $this->version, false );
+		// wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/hansefair-public.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'assets/hansefair.js', array( 'jquery' ), $this->version, false );
 
 	}
 
