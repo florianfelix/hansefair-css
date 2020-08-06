d7d6b9cda4fd773bd3948b86a7e9f5c2fc5541d8
florianfelix <florianfelixmeyer@gmail.com>
2020-08-06 15:44:35 +0200

    one

--- public/class-hansefair-public.php
+++ public/class-hansefair-public.php
@@ -73,7 +73,7 @@ class Hansefair_Public {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . '/assets/hansefair.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__, 1 ) . 'assets/hansefair.css', array(), $this->version, 'all' );
 
 	}
 
@@ -96,7 +96,7 @@ class Hansefair_Public {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . '/assets/hansefair.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'assets/hansefair.js', array( 'jquery' ), $this->version, false );
 
 	}
 
